e3adb067f4f42e33f17fcc54b37857f850eb97c1
vexleet <abda70226@gmail.com>
2022-01-09 13:25:32 +0100

    add on load open window

--- main.js
+++ main.js
@@ -1,22 +1,57 @@
-const { app, BrowserWindow } = require('electron')
-const path = require("path");
+const { app, BrowserWindow, ip } = require('electron')
+const fetch = require('electron-fetch').default
 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+function LoLCountdown() {
+  let win = undefined;
+
+  this.createWindow = () => {
+    win = new BrowserWindow({
+      width: 350,
+      height: 105,
+      frame: false,
+      type: 'toolbar'
+    })
+
+    win.loadFile('index.html');
+
+    // app.dock.hide();
+    win.setAlwaysOnTop(true, "floating");
+    win.setVisibleOnAllWorkspaces(true);
+    win.setFullScreenable(false);
+
+    // win.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true });
+    // win.setAlwaysOnTop(true, 'normal');
+    // win.setFullScreenable(false);
+    //
+    // win.moveTop();
+  }
+
+  this.closeAppWindow = () => {
+    if(win) {
+      win.close()
     }
-  })
-
-  win.loadFile('index.html')
+  }
 }
 
+app.commandLine.appendSwitch('ignore-certificate-errors', true);
+
 app.whenReady().then(() => {
-  createWindow()
+  const lolChampWindow = new LoLCountdown();
+  setInterval(() => {
+    fetch('https://127.0.0.1:2999/liveclientdata/allgamedata')
+      .then((res) => res.json())
+      .then((data) => {
+        if (data && BrowserWindow.getAllWindows().length === 0) {
+          console.log('Opening Window...')
+          lolChampWindow.createWindow()
+        }
+      }).catch((e) => {
+        if(BrowserWindow.getAllWindows().length === 1){
+          console.log('Closing Window...')
+          lolChampWindow.closeAppWindow()
+        }
+      });
+  }, 1500)
 })
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on('window-all-closed', e => e.preventDefault() )
